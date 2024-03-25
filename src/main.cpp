bb280842c41b61e37e5abdae48d71d737a7c4a80
IshaqZa <IshaqZarour9@gmail.com>
2024-03-25 10:48:11 +0300

    Window Setup In main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,51 +1,47 @@
-#include <iostream>
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
+#include <iostream>
+#include <string>
+void framebuffer_size_callback(GLFWwindow* window, int width, int height){
 
-using namespace std; 
-
-void framebuffer_size_callback(GLFWwindow* window, int width, int height)
-{
     glViewport(0, 0, width, height);
+
 }
 
-int main()
-{
-    if (!glfwInit())
-    {
-        cout << "Failed to initialize GLFW" << endl;
-        return -1;
-    }
+int main(){
 
-    glfwWindowHint(GLFW_SAMPLES, 4);
+    glfwInit();
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-    GLFWwindow* window;
-    window = glfwCreateWindow(800, 600, "Hello, Window!", NULL, NULL);
-    if (window == NULL)
-    {
-        cout << "Failed to open GLFW window" << endl;
-        return -1;
+    GLFWwindow *window = glfwCreateWindow(800, 600, "Hello, Window!", NULL, NULL);
+    if(window == NULL){
+        std::cout << "There was an error creating the window" << std::endl;
+        glfwTerminate();
+        return EXIT_FAILURE;
     }
+
     glfwMakeContextCurrent(window);
 
-    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
-    {
-        cout << "Failed to initialize GLAD" << endl;
-        return -1;
+    if(!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)){
+        std::cout << "Failed to initialize GLAD" << std::endl;
+        return EXIT_FAILURE;
     }
 
-    glViewport(0, 0, 800, 600);
     glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
 
-    while(!glfwWindowShouldClose(window))
-    {
+    while(!glfwWindowShouldClose(window)){
+
+        
+
         glfwSwapBuffers(window);
-        glfwPollEvents();    
+        glfwPollEvents();
+
     }
 
     glfwTerminate();
-    return 0;
+    return EXIT_SUCCESS;
+
+    
 }
