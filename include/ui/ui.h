03e1395e3ce3b2a90bd436dd791f2de22764b67f
IshaqZa <137885236+IshaqZa@users.noreply.github.com>
2024-04-19 14:01:15 +0300

    Refactored ui.h to make a MenuElement parent class

--- include/ui/ui.h
+++ include/ui/ui.h
@@ -10,21 +10,52 @@
 #define RGBA_TYPE 0
 #define IMAGE_TYPE 1
 
-template<typename returnValue, typename... Args>
-class button {
+class MenuElement {
 
-    private:
+    protected:
         GLuint index;
         std::string text;
         int renderType = RGBA_TYPE;
         GLfloat xCoor, yCoor;
         GLfloat width, height;
         GLfloat rColor, gColor, bColor, alpha;
+
+    public:
+
+        MenuElement(std::vector<GLfloat> &vertices, std::vector<GLuint> &indices,GLuint globalIndex, std::string text, GLfloat xCoor, GLfloat yCoor, GLfloat width, GLfloat height):
+            index(globalIndex),  text(text), xCoor(xCoor), yCoor(yCoor), width(width), height(height){};
+
+        void setPos(GLfloat xCoor, GLfloat yCoor){
+            this->xCoor = xCoor;
+            this->yCoor = yCoor;
+        }
+        void setSize(GLfloat xCoor, GLfloat yCoor){
+            this->width = width;
+            this->height = height;
+        }
+        
+        void setColor(GLfloat rColor, GLfloat gColor, GLfloat bColor, GLfloat alpha){
+            this->rColor = rColor;
+            this->gColor = gColor;
+            this->bColor = bColor;
+            this->alpha = alpha;
+        }
+
+        void setRenderType(int value ,GLuint location){ glUniform1i(location, value); }
+        void setText(std::string text){ this->text = text; }
+        virtual void draw() = 0;
+
+};
+
+template<typename returnValue, typename... Args>
+class button : public MenuElement{
+
+    private:
         std::function<returnValue(Args...)> onClickAction;
 
     public:
         button(std::vector<GLfloat> &vertices, std::vector<GLuint> &indices,GLuint globalIndex, std::string text, GLfloat xCoor, GLfloat yCoor, GLfloat width, GLfloat height):
-            index(globalIndex),  text(text), xCoor(xCoor), yCoor(yCoor), width(width), height(height){
+            MenuElement(vertices, indices, globalIndex, text, xCoor, yCoor, width, height){
 
             vertices.insert(vertices.end(), {
 
@@ -49,26 +80,10 @@ class button {
 
             });
         };
-        void setText(std::string text){ this->text = text; }
+        
         void onClick(returnValue (*action)(Args...)){ onClickAction = action; }
-        void setPos(GLfloat xCoor, GLfloat yCoor){
-            this->xCoor = xCoor;
-            this->yCoor = yCoor;
-        }
-        void setSize(GLfloat xCoor, GLfloat yCoor){
-            this->width = width;
-            this->height = height;
-        }
-        void setColor(GLfloat rColor, GLfloat gColor, GLfloat bColor, GLfloat alpha){
-            this->rColor = rColor;
-            this->gColor = gColor;
-            this->bColor = bColor;
-            this->alpha = alpha;
-        }
-        void setRenderType(int value ,GLuint location){
-            glUniform1i(location, value);
-        }
-        void draw(){
+        
+        void draw() override{
             glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
         }
         returnValue invoke(Args... args){ return onClickAction(args...); }
