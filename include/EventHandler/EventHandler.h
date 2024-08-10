3fb803625a9325e26328d4bfce45504b02dc3034
IshaqZa <137885236+IshaqZa@users.noreply.github.com>
2024-08-10 16:24:08 +0300

    start of mouse click up and down imp

--- include/EventHandler/EventHandler.h
+++ include/EventHandler/EventHandler.h
@@ -13,7 +13,8 @@ private:
         std::shared_ptr<MenuElement> element;
         std::function<void()> action;
     };
-    std::vector<ActionElement> onClickElements;
+    std::vector<ActionElement> onClickDownElements;
+    std::vector<ActionElement> onClickUpElements;
     std::vector<ActionElement> OnHoverEnterElements;
     std::vector<ActionElement> OnHoverLeaveElements;
     bool isHovered = false;
@@ -22,11 +23,13 @@ private:
 
     GLfloat normalizeX(GLfloat value, GLfloat width);
     GLfloat normalizeY(GLfloat value, GLfloat height);
-    void processOnClick(GLFWwindow* window);
+    void processOnMouseDown(GLFWwindow* window);
+    void processOnMouseUp(GLFWwindow* window);
     void processOnHoverEnter(GLFWwindow* window);
     void processOnHoverLeave(GLFWwindow* window);
 public:
-    void addOnClickElement(std::shared_ptr<MenuElement> element, std::function<void()> action);
+    void addOnClickDownElement(std::shared_ptr<MenuElement> element, std::function<void()> action);
+    void addOnClickUpElement(std::shared_ptr<MenuElement> element, std::function<void()> action);
     void addOnHoverEnterElement(std::shared_ptr<MenuElement> element, std::function<void()> action);
     void addOnHoverLeaveElement(std::shared_ptr<MenuElement> element, std::function<void()> action);
     void processInputs(GLFWwindow* window);
--- src/imp/EventHandler/EventHandler.cpp
+++ src/imp/EventHandler/EventHandler.cpp
@@ -1,8 +1,10 @@
 #include "EventHandler/EventHandler.h"
-void EventHandler::addOnClickElement(std::shared_ptr<MenuElement> element, std::function<void()> action){
+void EventHandler::addOnClickDownElement(std::shared_ptr<MenuElement> element, std::function<void()> action){
+    onClickDownElements.push_back({element, action});
+}
 
-    onClickElements.push_back({element, action});
-
+void EventHandler::addOnClickUpElement(std::shared_ptr<MenuElement> element, std::function<void()> action){
+    onClickUpElements.push_back({element, action});
 }
 
 void EventHandler::addOnHoverEnterElement(std::shared_ptr<MenuElement> element, std::function<void()> action){
@@ -20,13 +22,11 @@ GLfloat EventHandler::normalizeY(GLfloat value, GLfloat height){
     return 1.0f - 2.0f * value / height;
 }
 
-void EventHandler::processOnClick(GLFWwindow* window){
-
-    static int previousMouseButtonState = GLFW_RELEASE;
+void EventHandler::processOnMouseDown(GLFWwindow* window){
     
     int currentMouseButtonState = glfwGetMouseButton(window, GLFW_MOUSE_BUTTON_LEFT);
 
-    if(currentMouseButtonState == GLFW_PRESS && previousMouseButtonState == GLFW_RELEASE){
+    if(currentMouseButtonState == GLFW_PRESS){
         double x, y;
         glfwGetCursorPos(window, &x, &y);
 
@@ -38,16 +38,38 @@ void EventHandler::processOnClick(GLFWwindow* window){
 
         glm::vec2 pos(normalizedX, normalizedY);
         
-        for(const auto& clickable : onClickElements){
+        for(const auto& clickable : onClickDownElements){
             if(clickable.element->contains(pos)){
                 clickable.action();
                 break;
             }
         }
-
-        previousMouseButtonState = currentMouseButtonState;
     }
-    if(currentMouseButtonState == GLFW_RELEASE) previousMouseButtonState = GLFW_RELEASE;
+}
+
+void EventHandler::processOnMouseUp(GLFWwindow* window){
+    
+    int currentMouseButtonState = glfwGetMouseButton(window, GLFW_MOUSE_BUTTON_LEFT);
+
+    if(currentMouseButtonState == GLFW_RELEASE){
+        double x, y;
+        glfwGetCursorPos(window, &x, &y);
+
+        int width, height;
+        glfwGetWindowSize(window, &width, &height);
+
+        GLfloat normalizedX = normalizeX(x, width);
+        GLfloat normalizedY = normalizeY(y, height);
+
+        glm::vec2 pos(normalizedX, normalizedY);
+        
+        for(const auto& clickable : onClickUpElements){
+            if(clickable.element->contains(pos)){
+                clickable.action();
+                break;
+            }
+        }
+    }
 }
 
 void EventHandler::processOnHoverEnter(GLFWwindow* window){
@@ -96,7 +118,8 @@ void EventHandler::processOnHoverLeave(GLFWwindow* window){
 }
 
 void EventHandler::processInputs(GLFWwindow* window){
-    processOnClick(window);
+    processOnMouseDown(window);
+    processOnMouseUp(window);
     std::cout << "Processed user clicks" << std::endl;
     processOnHoverEnter(window);
     processOnHoverLeave(window);
